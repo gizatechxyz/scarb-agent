c9f18290193d50c5f030aa9beb9801f02b33c497
raphaelDkhn <113879115+raphaelDkhn@users.noreply.github.com>
2024-09-07 11:10:10 +0300

    restructure

--- scarb-agent/scarb-agent-new/templates.rs
+++ scarb-agent/scarb-agent-new/templates.rs
@@ -5,46 +5,46 @@ pub fn get_template_engine() -> handlebars::Handlebars<'static> {
     registry
         .register_template_string(
             "dockerfile",
-            include_str!("templates/python/Dockerfile.hbs").to_owned(),
+            include_str!("templates/Dockerfile.hbs").to_owned(),
         )
         .unwrap();
     registry
         .register_template_string(
             "requirements",
-            include_str!("templates/python/requirements.hbs").to_owned(),
+            include_str!("templates/requirements.hbs").to_owned(),
         )
         .unwrap();
     registry
-        .register_template_string("main", include_str!("templates/python/main.hbs").to_owned())
+        .register_template_string("main", include_str!("templates/main.hbs").to_owned())
         .unwrap();
     registry
         .register_template_string(
             "pre-commit",
-            include_str!("templates/python/.pre-commit-config.hbs").to_owned(),
+            include_str!("templates/.pre-commit-config.hbs").to_owned(),
         )
         .unwrap();
     registry
         .register_template_string(
             "gitignore",
-            include_str!("templates/python/.gitignore.hbs").to_owned(),
+            include_str!("templates/.gitignore.hbs").to_owned(),
         )
         .unwrap();
     registry
         .register_template_string(
             "readme",
-            include_str!("templates/python/README.hbs").to_owned(),
+            include_str!("templates/README.hbs").to_owned(),
         )
         .unwrap();
     registry
         .register_template_string(
             "cloudbuild",
-            include_str!("templates/python/cloudbuild.hbs").to_owned(),
+            include_str!("templates/cloudbuild.hbs").to_owned(),
         )
         .unwrap();
     registry
         .register_template_string(
             "run-service",
-            include_str!("templates/python/run-service.hbs").to_owned(),
+            include_str!("templates/run-service.hbs").to_owned(),
         )
         .unwrap();
     registry
---
+++ scarb-agent/scarb-agent-new/templates/Dockerfile.hbs
@@ -0,0 +1,12 @@
+FROM python:3.12-slim
+
+WORKDIR /opt/app
+
+COPY ./python/requirements.txt requirements.txt
+
+RUN pip install --no-cache-dir uv
+RUN uv pip install --system --no-cache-dir --upgrade -r requirements.txt
+
+COPY ./python/src ./
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8888"]
